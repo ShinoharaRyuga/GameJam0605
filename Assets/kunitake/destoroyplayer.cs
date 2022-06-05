468d01d174b6edfc835beb4733ff9a7a22b51808
kunitake_daisuke_VGAT２２ <kariruka92@gmail.com>
2022-06-05 15:31:19 +0900

    プレイヤーを消す

---
+++ Assets/kunitake/destoroyplayer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destoroyplayer : MonoBehaviour
+{
+
+    public void DesObject()
+    {
+        Destroy(this.gameObject);//ƒvƒŒƒCƒ„[‚ğÁ‚·
+    }
+}
