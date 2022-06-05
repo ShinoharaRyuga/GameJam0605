732fd58375fa9a5f0cf7b134bcf5ea0cb38798a7
SinoharaRyuga <raitan3170@gmail.com>
2022-06-06 04:22:22 +0900

    フェードイン・アウトのプレハブを修正　プレイヤーの攻撃とHPを動作するように修正

--- Assets/OkamotoRyota/Script/PlayerHP.cs
+++ Assets/OkamotoRyota/Script/PlayerHP.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
     public int HP;//playersHP
     public int HPmax = 3;//PlayerMAXHP = 3
+    [Tooltip("Slider"), SerializeField] Slider _hpSlider = default;
+    [Tooltip("InfoText Ç…ï\é¶Ç∑ÇÈ Text"), SerializeField] Text _infoText = default;
+
     void Start()
     {
         HP = HPmax;//ç≈èâÇ…playerÇÃHPÇmaxÇÃèÛë‘Ç…Ç∑ÇÈ
-
+        _hpSlider.maxValue = HPmax;
+        _hpSlider.value = HPmax;
     }
 
     // Update is called once per frame
