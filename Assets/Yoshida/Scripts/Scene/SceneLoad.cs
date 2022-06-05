c3e4583097882f943abc9eee6b949a2e8182704c
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-06-05 15:09:50 +0900

    シーン遷移制作中

--- Assets/Yoshida/Scripts/Scene/SceneLoad.cs
+++ Assets/Yoshida/Scripts/Scene/SceneLoad.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+
+/// <summary>
+/// Button にアタッチして使うクラス。名前を指定してシーンをロードする
+/// </summary>
 public class SceneLoad : MonoBehaviour
-{
+{   
     
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log($"{sceneName}シーンをロード");
+    }
 }
