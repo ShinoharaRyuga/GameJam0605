842d5f0081a19b392d474f9f197137e5c6ca999e
SinoharaRyuga <raitan3170@gmail.com>
2022-06-05 15:41:51 +0900

    ゲームシーンに遷移したらプレイヤーを生成する機能を作成

--- Assets/shinohara/GameManager.cs
+++ Assets/shinohara/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = default;
     [SerializeField, Tooltip("各ステージの敵の数")] int[] _stageEnemyCount = default;
     [SerializeField, Tooltip("フェードをさせるオブジェクト")] GameObject _fadeObj = default;
+    [SerializeField] GameObject _playerPrefab = default;
+    PlayerHP _playerHP;
+    Attackspace _attackspace;
     /// <summary>現在のステージ </summary>
     int _currentStage = 0;
+    public PlayerHP PlayerHP { get => _playerHP; set => _playerHP = value; }
+    public Attackspace Attackspace { get => _attackspace; set => _attackspace = value; }
 
     // GameObject _player => GetComponent<GameObject>();
     private void Awake()
@@ -20,20 +26,21 @@ public class GameManager : MonoBehaviour
         else
         {
             Instance = this;
+            SceneManager.sceneLoaded += GameSceneLoad;
             DontDestroyOnLoad(this);
         }
     }
-    void Start()
+ 
+    /// <summary>ゲームシーンに遷移したらプレイヤーを生成する </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    public void GameSceneLoad(Scene scene, LoadSceneMode mode)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (scene.name == "GameScene")
         {
-            Instantiate(_fadeObj);
+            var player = Instantiate(_playerPrefab, Vector2.zero, Quaternion.identity);
+            _playerHP = player.GetComponent<PlayerHP>();
+            _attackspace = player.GetComponent<Attackspace>();
         }
     }
 
