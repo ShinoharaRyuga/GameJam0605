e80aba3791c87ddf61f5b219fe123418a4c661a2
SinoharaRyuga <raitan3170@gmail.com>
2022-06-05 15:03:37 +0900

    ゲームシーンに遷移したらプレイヤーを生成出来るようにした

--- Assets/shinohara/GameManager.cs
+++ Assets/shinohara/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = default;
     [SerializeField, Tooltip("各ステージの敵の数")] int[] _stageEnemyCount = default;
     [SerializeField, Tooltip("フェードをさせるオブジェクト")] GameObject _fadeObj = default;
+    [SerializeField, Tooltip("プレイヤーのプレハブ")] GameObject _playerPrefab = default;
+    PlayerHP _playerHP;
+    Attackspace _attackspace;
     /// <summary>現在のステージ </summary>
     int _currentStage = 0;
+    public PlayerHP PlayerHP { get => _playerHP; }
+    public Attackspace Attackspace { get => _attackspace; }
 
-    // GameObject _player => GetComponent<GameObject>();
     private void Awake()
     {
         if (Instance)
@@ -20,6 +25,7 @@ public class GameManager : MonoBehaviour
         else
         {
             Instance = this;
+            SceneManager.sceneLoaded += GameSceneLoad;
             DontDestroyOnLoad(this);
         }
     }
@@ -28,12 +34,14 @@ public class GameManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>遷移した時にプレイヤーを生成する </summary>
+    public void GameSceneLoad(Scene scene, LoadSceneMode mode)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (scene.name == "GameScene")
         {
-            Instantiate(_fadeObj);
+            var player = Instantiate(_playerPrefab, Vector2.zero, Quaternion.identity);
+            _playerHP = player.GetComponent<PlayerHP>();
+            _attackspace = player.GetComponent<Attackspace>();
         }
     }
 
@@ -46,5 +54,11 @@ public class GameManager : MonoBehaviour
         {
             _currentStage++;
         }
+    }
+
+    /// <summary>フェードオブジェクトを作成する </summary>
+    public void InstantiateFadeObj()
+    {
+        Instantiate(_fadeObj);
     }
 }
